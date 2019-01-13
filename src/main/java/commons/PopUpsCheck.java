1f2f36e0824f39423364dc14e4b0fa88e96497f9
hjx19 <hjx19881022@126.com>
2019-01-13 20:46:37 +0800

    处理非预期弹框

--- src/main/java/commons/PopUpsCheck.java
+++ src/main/java/commons/PopUpsCheck.java
@@ -1,8 +1,6 @@
 package commons;
 
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.WebElement;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
@@ -11,39 +9,35 @@ import java.util.Observable;
 /**
  * 后台线程一直跑，检查到有弹窗，就notify, 同时带上要点击的元素
  */
-public class PopUpsStatus extends Observable implements Runnable {
-    private AppiumDriver<?> driver;
-    private WebElement element;
-    private ArrayList windowBlack;
+public class PopUpsCheck extends Observable implements Runnable {
 
-    public PopUpsStatus(AppiumDriver<?> driver){
+    private AppiumDriver<?> driver;
+    private ArrayList<String> windowBlack;
+
+    public PopUpsCheck(AppiumDriver<?> driver){
         this.driver = driver;
         YamlOps utilConf = new YamlOps("..\\AutoUITest\\src\\main\\resources\\Util_Conf.yml");
         try {
             Map<String, Object> utilContent = utilConf.getContent();
-            windowBlack = (ArrayList) utilContent.get("WINDOW_BLACK");
+            windowBlack = (ArrayList<String>) utilContent.get("WINDOW_BLACK");
         } catch (IOException e) {
             LoggerConf.logobject.severe("获取Util_Conf配置文件内容失败");
         }
     }
 
+    public AppiumDriver<?> getDriver() {
+        return driver;
+    }
 
     @Override
     public void run() {
+        for (String s : windowBlack){
+            if (driver.findElementsByXPath(s) != null){
+                setChanged();
+                notifyObservers(s);
+            }
+        }
+        clearChanged();
 
     }
-//    private boolean changed = false;
-//跑后台线程，出现弹框后调setchanged
-//    @Override
-//    protected synchronized void setChanged() {
-//        changed = true;
-//    }
-//
-//    @Override
-//    public void notifyObservers(Object arg) {
-//        if(changed){
-//            update(arg);
-//        }
-//        changed=false;
-//    }
 }
--- src/main/java/commons/PopUpsOperate.java
+++ src/main/java/commons/PopUpsOperate.java
@@ -1,12 +1,20 @@
 package commons;
 
+import io.appium.java_client.AppiumDriver;
+
 import java.util.Observable;
 import java.util.Observer;
 
 public class PopUpsOperate implements Observer {
+    private AppiumDriver<?> driver;
+
     @Override
-    public void update(Observable popUpsStatus, Object element) {
-//这里操作点击弹框
+    public void update(Observable observable, Object arg) {
+        if(observable instanceof PopUpsCheck){
+            PopUpsCheck popUpsCheck = (PopUpsCheck)observable;
+            driver = popUpsCheck.getDriver();
+            driver.findElementByXPath((String)arg).click();
+        }
 
     }
 }
